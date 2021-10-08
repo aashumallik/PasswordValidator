2f2f1eb1701121b6f5a3484ab39389a7e65a23fe
Aashutosh Mallik <Aashu.Mallik@gofastpath.com>
2021-10-08 11:28:09 -0500

    made some changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace PasswordValidator
     {
         static void Main(string[] args)
         {
-            string password = "Password";
+            string password = "cunt";
             //Console.WriteLine("You have 3 tries to guess the password");
             int guess_count = 0;
             int guess_limit = 3;
@@ -14,11 +14,11 @@ namespace PasswordValidator
             bool hasGuess = true;
 
             string guess_password ="";
-            while (guess_password != password & hasGuess)
+            while (guess_password != password & hasGuess) //If the entered password is not equal to the hardcoded password and the user has guesses left
             {
                 if (guess_count < guess_limit)
                 {
-                    Console.WriteLine("Start guessing: ");
+                    Console.WriteLine("Start guessing fucker: ");
                     guess_password = Console.ReadLine();
                     guess_count++;
                 }
@@ -29,14 +29,12 @@ namespace PasswordValidator
             }
             if (!hasGuess)
             {
-                Console.WriteLine("You're out of tries");
+                Console.WriteLine("You ran out of tries and the log has been sent to the FBI");
             }
             else
             {
                 Console.WriteLine("You're in");
             }
-
-
         }
     }
 }
